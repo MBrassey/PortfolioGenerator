6994bccd4e34503e45a9476a0adc18fd4ac47f11
luc1dLife <luc1d_life@protonmail.com>
2020-09-23 08:58:34 -0600

    + Generate HTML File / Modularize js

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log("================");
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach(function (profileItem) {
-        console.log(profileItem);
-    });
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
