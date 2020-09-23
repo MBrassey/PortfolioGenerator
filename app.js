5fbf7fba31205e54e92151190a2fc4a7143dd985
luc1dLife <luc1d_life@protonmail.com>
2020-09-22 22:53:21 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = (profileDataArr) => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log("================");
+
+    profileDataArr.forEach(function (profileItem) {
+        console.log(profileItem);
+    });
+};
+
+printProfileData(profileDataArgs);
