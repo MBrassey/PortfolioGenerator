e5f3943e67be9627d9f87c34ccd7dd35561232aa
luc1dLife <luc1d_life@protonmail.com>
2020-09-24 08:34:41 -0600

    + Validation

--- app.js
+++ app.js
@@ -15,18 +15,47 @@ const promptUser = () => {
         {
             type: "input",
             name: "name",
-            message: "What is your name?",
+            message: "What is your name? (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            },
         },
         {
             type: "input",
             name: "github",
-            message: "Enter your GitHub Username",
+            message: "Enter your GitHub Username: (Required)",
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub Username!");
+                    return false;
+                }
+            },
         },
         {
-            type: "input",
-            name: "about",
-            message: "Provide some information about yourself:",
-        },
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section?',
+            default: true
+          },
+          {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+              if (confirmAbout) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          }
     ]);
 };
 
@@ -51,6 +80,14 @@ const promptProject = (portfolioData) => {
                 type: "input",
                 name: "description",
                 message: "Provide a description of the project (Required)",
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter a description for your Project!");
+                        return false;
+                    }
+                },
             },
             {
                 type: "checkbox",
@@ -62,6 +99,14 @@ const promptProject = (portfolioData) => {
                 type: "input",
                 name: "link",
                 message: "Enter the GitHub link to your project. (Required)",
+                validate: (nameInput) => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("Please enter the project's GitHub link!");
+                        return false;
+                    }
+                },
             },
             {
                 type: "confirm",
