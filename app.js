9d2ef1e40f8b47484eb2b6063a6944987f2e99b2
luc1dLife <luc1d_life@protonmail.com>
2020-09-25 15:28:31 -0600

    Completed Template

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -132,7 +124,13 @@ const promptProject = (portfolioData) => {
 };
 
 promptUser()
-    .then(promptProject)
-    .then((portfolioData) => {
-        console.log(portfolioData);
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
     });
+  });
